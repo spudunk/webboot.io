2da38720878c962e291fb2db493587cc0319bc94
spudunk <hicksme@gmail.com>
2023-06-30 14:58:08 -0700

    fix dot env setup

--- functions/index.js
+++ functions/index.js
@@ -1,54 +1,53 @@
-/* eslint-disable */
+/* eslint-disable require-jsdoc */
+
 // const functions = require("firebase-functions");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const { setGlobalOptions } = require("firebase-functions/v2");
-setGlobalOptions({ maxInstances: 10 });
+const {setGlobalOptions} = require("firebase-functions/v2");
+setGlobalOptions({maxInstances: 10});
 
-const { onValueCreated } = require("firebase-functions/v2/database");
+const {onValueCreated} = require("firebase-functions/v2/database");
 
 const postmark = require("postmark");
-var client = new postmark.ServerClient(process.env.POSTMARKKEY);
+const client = new postmark.ServerClient(process.env.POSTMARKKEY);
 
-// console.log("cloud function running...");
 
 exports.formsubmit = onValueCreated("/submissions/{id}", (event) => {
-  console.log(event);
   return sendEmail(event.data.val());
 });
 
-// Send welcome email to new users
 function sendEmail(data) {
-  console.log("function sendEmail triggered");
-
   const confirmMailOptions = {
     MessageStream: "outbound",
     From: "chris@webboot.io",
     To: data.email,
     Subject: "Thanks for your submission",
-    TextBody: `Hi ${data.name}! We have received your contact request and will get back to you withing a few days.`,
+    TextBody: `Hi ${data.name}!\n` +
+    `We have received your contact request and ` +
+    `will get back to you within a few days.`,
   };
 
   const captureMailOptions = {
     MessageStream: "outbound",
     From: "submissions@webboot.io",
     To: "chris@webboot.io",
-    Subject: `Form submission from ${data.name} at ${
-      data.company || "(no company)"
-    }`,
-    TextBody: `name: ${data.name}
-email: ${data.email}
-company: ${data.company}
-location: ${data.location}
-phone: ${data.phone}`,
+    Subject: `Form submission from ${data.name || "(no name)"} ` +
+    `at ${data.company || "(no company)"}`,
+    TextBody: `name: ${data.name}\n` +
+    `email: ${data.email}\n` +
+    `company: ${data.company}\n` +
+    `location: ${data.location}\n` +
+    `phone: ${data.phone}`,
   };
 
   client
-    .sendEmail(confirmMailOptions)
-    .then(() => console.log(`Confirmation email sent to ${data.email}`))
-    .catch((e) => console.error(`Error sending capture email: ${e}`));
+      .sendEmail(confirmMailOptions)
+      .then(() => console.log(`Confirmation email sent to ${data.email}`))
+      .catch((e) => console.error(`Error sending capture email: ${e}`));
 
   client
-    .sendEmail(captureMailOptions)
-    .then(() => console.log("Capture email sent"))
-    .catch((e) => console.error(`Error sending capture email: ${e}`));
+      .sendEmail(captureMailOptions)
+      .then(() => console.log("Capture email sent"))
+      .catch((e) => console.error(`Error sending capture email: ${e}`));
 }

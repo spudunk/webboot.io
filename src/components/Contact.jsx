8cb73aa7791495f36d882c9a232869f0c096a9ba
spudunk <hicksme@gmail.com>
2023-07-24 18:23:42 -0700

    moved to liv

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,10 @@
 import styles from "../styles/Contact.module.css";
 import { useState } from "react";
-import initFirebase from "../../firebase/initFirebase";
-import { getDatabase, ref, push } from "firebase/database";
-
+import { db } from "../lib/initFirebase";
+import { ref, push } from "firebase/database";
 
 export default function Contact() {
-  const app = initFirebase();
-  const db = getDatabase(app);
-  
+
   const initialFormData = {
     email: "",
     name: "",
@@ -28,8 +25,7 @@ export default function Contact() {
     }
     setError("");
     setSubmitting(true);
-    push(ref(db, 'submissions'),formData)
-    .then((r) => {
+    push(ref(db, "submissions"), formData).then((r) => {
       setSubmitting(false);
       setFormData(initialFormData);
       setSubmitted(true);
@@ -37,7 +33,7 @@ export default function Contact() {
         setSubmitted(false);
       }, 5000);
       // console.log(r);
-    })
+    });
   }
 
   const handleChange = (e) => {
@@ -50,70 +46,75 @@ export default function Contact() {
   };
 
   return (
-        <div className={styles.content}>
-          <form action="" className={styles.form}>
-            <div className={styles.formitem}>
-              <label htmlFor="email">Email
-              <input
-                onChange={handleChange}
-                name="email"
-                type="email"
-                value={formData.email}
-              />
-              </label>
-            </div>
-            <div className={styles.formitem}>
-              <label htmlFor="name">Name
-              <input
-                onChange={handleChange}
-                name="name"
-                type="text"
-                value={formData.name}
-              />
-              </label>
-            </div>
-            <div className={styles.formitem}>
-              <label htmlFor="phone">Phone
-              <input
-                onChange={handleChange}
-                name="phone"
-                type="phone"
-                value={formData.phone}
-              />
-              </label>
-            </div>
-            <div className={styles.formitem}>
-              <label htmlFor="company">Company
-              <input
-                onChange={handleChange}
-                name="company"
-                type="text"
-                value={formData.company}
-              />
-              </label>
-            </div>
-            <div className={styles.formitem}>
-              <label htmlFor="phone">Location
-              <input
-                onChange={handleChange}
-                name="location"
-                type="text"
-                value={formData.location}
-              />
-              </label>
-            </div>
-            <button
-              className={`btn btn-primary ${styles.formbtn}`}
-              onClick={handleSubmit}
-            >
-              Submit
-            </button>
-            <div className={styles.formmsg}>
-              {submitting && <div>Submitting Form...</div>}
-              {submitted && <div>Form Submitted!</div>}
-              {error}
-            </div>
-          </form>
+    <div className={styles.content}>
+      <form action="" className={styles.form}>
+        <div className={styles.formitem}>
+          <label htmlFor="email">
+            Email
+            <input
+              onChange={handleChange}
+              name="email"
+              type="email"
+              value={formData.email}
+            />
+          </label>
         </div>
+        <div className={styles.formitem}>
+          <label htmlFor="name">
+            Name
+            <input
+              onChange={handleChange}
+              name="name"
+              type="text"
+              value={formData.name}
+            />
+          </label>
+        </div>
+        <div className={styles.formitem}>
+          <label htmlFor="phone">
+            Phone
+            <input
+              onChange={handleChange}
+              name="phone"
+              type="phone"
+              value={formData.phone}
+            />
+          </label>
+        </div>
+        <div className={styles.formitem}>
+          <label htmlFor="company">
+            Company
+            <input
+              onChange={handleChange}
+              name="company"
+              type="text"
+              value={formData.company}
+            />
+          </label>
+        </div>
+        <div className={styles.formitem}>
+          <label htmlFor="phone">
+            Location
+            <input
+              onChange={handleChange}
+              name="location"
+              type="text"
+              value={formData.location}
+            />
+          </label>
+        </div>
+        <button
+          className={`btn btn-primary ${styles.formbtn}`}
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+        <div className={styles.formmsg}>
+          {submitting && <div>Submitting Form...</div>}
+          {submitted && <div>Form Submitted!</div>}
+          {error}
+        </div>
+      </form>
+    </div>
   );
 }

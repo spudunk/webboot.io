afacb09d1071ba5e4ddced112ee02f114a97008e
spudunk <hicksme@gmail.com>
2023-09-13 11:15:50 -0700

    outline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,28 @@
 <script>
-
-
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
+<Header />
+
+<section id="hero">
+	<div />
+</section>
+
+<section id="about">
+	<div />
+</section>
+
+<section id="services">
+	<div />
+</section>
+
+<section id="credibility">
+	<div />
+</section>
+
+<section id="contact">
+	<div />
+</section>
+
+<Footer />

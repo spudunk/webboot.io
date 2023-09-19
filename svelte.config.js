7be7920c816b4c0bf7f300d9c7c477818f09aa5e
spudunk <hicksme@gmail.com>
2023-09-18 18:24:04 -0700

    switch to cloudflare pages adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-// import adapter from 'svelte-adapter-bun';
-// import { preprocessMeltUI } from '@melt-ui/pp';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-// import sequence from 'svelte-sequential-preprocessor';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: 
-	// sequence([
-		vitePreprocess(), 
-		// preprocessMeltUI()
-	// ]),
-
+	preprocess: vitePreprocess(),
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
 	}
 };
 

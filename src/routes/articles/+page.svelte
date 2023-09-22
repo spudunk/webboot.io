9be71355e12dc0074fd8bbd6f0d1c75675950909
Christopher Hicks <hickscme@gmail.com>
2023-09-21 20:13:22 -0700

    remove padding

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -3,11 +3,11 @@
 	const posts = fetchMarkdownPosts();
 </script>
 
-<h1 class="text-4xl">Articles</h1>
+<h1 class="text-4xl font-bold">Articles</h1>
 <ul class="mt-8 divide-y">
 	{#await posts then posts}
 		{#each posts as post}
-			<li class="py-4 pl-2">
+			<li class="py-4">
 				<a href={post.path}>
 					<h2 class="text-2xl font-semibold">{post.meta.title}</h2>
 					<p class="max-w-prose">{post.meta.description}</p>

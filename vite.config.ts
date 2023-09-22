0b15f4e7e673b4b1b62020f5ea204102af833351
Christopher Hicks <hickscme@gmail.com>
2023-09-21 21:10:07 -0700

    disable purge-css

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+// import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [
+		sveltekit(), 
+	// 	purgeCss({
+	// 	safelist: {
+	// 		// any selectors that begin with "hljs-" will not be purged
+	// 		greedy: [/^hljs-/],
+	// 	},
+	// })
+]
 });

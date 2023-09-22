a371b446b47e04aab93a359934aa0ce6484842a5
Christopher Hicks <hickscme@gmail.com>
2023-09-22 12:14:17 -0700

    cleanup

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,7 @@
 
 export const fetchMarkdownPosts =  () => {
-	// const allPostFiles = import.meta.glob('/src/articles/*.md');
 	const allPostFiles = import.meta.glob('/src/articles/*.md', {eager: true});
 	const iterablePostFiles = Object.entries(allPostFiles);
-
 	const allPosts = 
 		iterablePostFiles.map( (file) => {
       const [path, {metadata}] = file;
@@ -13,6 +11,5 @@ export const fetchMarkdownPosts =  () => {
 				path: postPath
 			};
 		});
-
 	return allPosts;
 };

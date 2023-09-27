53cfa1f8b8f5ed7528ee4b51ecf4a5005dee1d49
Christopher Hicks <hickscme@gmail.com>
2023-09-27 15:10:43 -0700

    update privacy policy

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -8,11 +8,7 @@
 	</p>
 	<p>
 		We use Your Personal data to provide and improve the Service. By using the Service, You agree to
-		the collection and use of information in accordance with this Privacy Policy. This Privacy
-		Policy has been created with the help of the <a
-			href="https://www.freeprivacypolicy.com/free-privacy-policy-generator/"
-			target="_blank">Free Privacy Policy Generator</a
-		>.
+		the collection and use of information in accordance with this Privacy Policy.
 	</p>
 	<h2>Interpretation and Definitions</h2>
 	<h3>Interpretation</h3>
@@ -170,71 +166,11 @@
 	</p>
 	<h4>Tracking Technologies and Cookies</h4>
 	<p>
-		We use Cookies and similar tracking technologies to track the activity on Our Service and store
-		certain information. Tracking technologies used are beacons, tags, and scripts to collect and
-		track information and to improve and analyze Our Service. The technologies We use may include:
+		We <strong>DO NOT</strong> use Cookies to track the activity on Our Service. We use the privacy focused 
+		analytics tool <a href="https://www.simpleanalytics.com/?referral=fawet">Simple Analytics</a> to 
+		track information and to improve and analyze Our Service. 
 	</p>
-	<ul>
-		<li>
-			<strong>Cookies or Browser Cookies.</strong> A cookie is a small file placed on Your Device. You
-			can instruct Your browser to refuse all Cookies or to indicate when a Cookie is being sent. However,
-			if You do not accept Cookies, You may not be able to use some parts of our Service. Unless you
-			have adjusted Your browser setting so that it will refuse Cookies, our Service may use Cookies.
-		</li>
-		<li>
-			<strong>Web Beacons.</strong> Certain sections of our Service and our emails may contain small
-			electronic files known as web beacons (also referred to as clear gifs, pixel tags, and single-pixel
-			gifs) that permit the Company, for example, to count users who have visited those pages or opened
-			an email and for other related website statistics (for example, recording the popularity of a certain
-			section and verifying system and server integrity).
-		</li>
-	</ul>
-	<p>
-		Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies remain on Your personal
-		computer or mobile device when You go offline, while Session Cookies are deleted as soon as You
-		close Your web browser. Learn more about cookies on the <a
-			href="https://www.freeprivacypolicy.com/blog/sample-privacy-policy-template/#Use_Of_Cookies_And_Tracking"
-			target="_blank">Free Privacy Policy website</a
-		> article.
-	</p>
-	<p>We use both Session and Persistent Cookies for the purposes set out below:</p>
-	<ul>
-		<li>
-			<p><strong>Necessary / Essential Cookies</strong></p>
-			<p>Type: Session Cookies</p>
-			<p>Administered by: Us</p>
-			<p>
-				Purpose: These Cookies are essential to provide You with services available through the
-				Website and to enable You to use some of its features. They help to authenticate users and
-				prevent fraudulent use of user accounts. Without these Cookies, the services that You have
-				asked for cannot be provided, and We only use these Cookies to provide You with those
-				services.
-			</p>
-		</li>
-		<li>
-			<p><strong>Cookies Policy / Notice Acceptance Cookies</strong></p>
-			<p>Type: Persistent Cookies</p>
-			<p>Administered by: Us</p>
-			<p>
-				Purpose: These Cookies identify if users have accepted the use of cookies on the Website.
-			</p>
-		</li>
-		<li>
-			<p><strong>Functionality Cookies</strong></p>
-			<p>Type: Persistent Cookies</p>
-			<p>Administered by: Us</p>
-			<p>
-				Purpose: These Cookies allow us to remember choices You make when You use the Website, such
-				as remembering your login details or language preference. The purpose of these Cookies is to
-				provide You with a more personal experience and to avoid You having to re-enter your
-				preferences every time You use the Website.
-			</p>
-		</li>
-	</ul>
-	<p>
-		For more information about the cookies we use and your choices regarding cookies, please visit
-		our Cookies Policy or the Cookies section of our Privacy Policy.
-	</p>
+
 	<h3>Use of Your Personal Data</h3>
 	<p>The Company may use Personal Data for the following purposes:</p>
 	<ul>

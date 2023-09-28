a136e2cd1212ff957b027eb9011fb3dbdde5aa14
Christopher Hicks <hickscme@gmail.com>
2023-09-28 14:49:25 -0700

    add check for updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,14 +13,15 @@
 				immediate: true,
 				onRegistered(r) {
 					// uncomment following code if you want check for updates
-					// r && setInterval(() => {
-					//    console.log('Checking for sw update')
-					//    r.update()
-					// }, 20000 /* 20s for testing purposes */)
-					console.log(`SW Registered: ${r}`);
+					r &&
+						setInterval(() => {
+							//    console.log('Checking for sw update')
+							r.update();
+						}, 5 * 60 * 1000 /* m * s * ms */);
+					// console.log(`SW Registered: ${r}`);
 				},
 				onRegisterError(error) {
-					console.log('SW registration error', error);
+					console.error('SW registration error', error);
 				}
 			});
 		}

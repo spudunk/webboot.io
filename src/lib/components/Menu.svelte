33046b9a124cd2b2a84f9172a57707f14ea6028e
Christopher Hicks <hickscme@gmail.com>
2023-09-27 17:10:40 -0700

    fix overlay

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -3,8 +3,6 @@
 	import { navigating } from '$app/stores';
 	export let toggled = false;
   export let padMenu = false;
-	const line =
-		'bg-cyan-50 w-7 sm:w-9 h-[.2rem]  sm:h-1 my-[.2rem] sm:my-1 rounded-full transition ease transform duration-300';
 	$: if ($navigating) {
 		menuToggled.set(false);
 	}
@@ -14,8 +12,8 @@
 <div
 	id="overlay"
 	class={`${
-		toggled ? 'fixed h-screen opacity-60' : 'hidden h-0 opacity-0'
-	} fixed -z-50 w-screen h-screen bg-cyan-900 top-0 left-0 transition-opacity ease-in duration-300`}
+		toggled ? 'h-screen opacity-60' : 'h-0 opacity-0'
+	} fixed -z-50 w-screen bg-cyan-900 top-0 left-0 transition-opacity ease-in duration-300`}
 	on:click={toggleMenu}
 	on:keyup={toggleMenu}
 />

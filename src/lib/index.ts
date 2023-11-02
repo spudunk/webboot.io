7eba9a2081f1d0763e85032ea2785c1c367c1ae5
Christopher Hicks <hickscme@gmail.com>
2023-11-02 13:58:23 -0700

    update article order

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,6 +7,18 @@ export const data = {
 };
 
 export const articles = {
+	process: {
+		slug: 'process',
+		title: 'Our Process',
+		heading: 'The Problem with Filling in the Blanks',
+		date: 'November 2, 2023',
+		author: 'Christopher Hicks',
+		img: 'https://webboot.io/og/process-sm.png',
+		description:
+			"Why Refined Pitches Lead to More Effective Websites. \
+			Exactly how our process works. What's inside our blueprint. \
+			How to get more value from your website. And more. "
+	},
 	guide: {
 		slug: 'guide',
 		title: 'Web Sucess Guide',
@@ -18,22 +30,10 @@ export const articles = {
 			"We're offering our entire process as a DIY guide with an unbelievable gaurantee. \
 			For a limited time we're including additional discounts, consulting, and resources."
 	},
-	process: {
-		slug: 'process',
-		title: 'Our Process',
-		heading: 'Our process for building brands online',
-		date: 'November 2, 2023',
-		author: 'Christopher Hicks',
-		img: 'https://webboot.io/og/process-sm.png',
-		description: 
-			"Exactly how our process works. What's inside our blueprint. \
-			How to get more value from your website. And more. "
-
-	},
 	password: {
 		slug: 'password',
-		title: 'Password technique',
-		heading: 'My password technique revealed!',
+		title: 'Password Technique',
+		heading: 'My Password Technique Revealed!',
 		date: 'September 22, 2023',
 		author: 'Christopher Hicks',
 		img: 'https://webboot.io/og/password-sm.png',
@@ -44,56 +44,52 @@ export const articles = {
 	},
 	whywebapps: {
 		slug: 'whywebapps',
-		title: 'Why web apps',
-		heading: 'Why we only build web apps',
+		title: 'Why Web Apps',
+		heading: 'Why We Only Build Web Apps',
 		date: 'August 11, 2023',
 		author: 'Christopher Hicks',
 		img: 'https://webboot.io/og/whywebapps-sm.png',
 		description:
 			"Read this to learn why we DO NOT use Wordpress. What is a web app? What makes web apps better? \
 			Do you need to switch? We're sharing the candid TRUTH. Find out now."
-	},
-	
+	}
 };
 
-
-
-
 function fallbackCopyTextToClipboard(text: string) {
-  const textArea = document.createElement('textarea');
-  textArea.value = text;
+	const textArea = document.createElement('textarea');
+	textArea.value = text;
 
-  // Avoid scrolling to bottom
-  textArea.style.top = '0';
-  textArea.style.left = '0';
-  textArea.style.position = 'fixed';
+	// Avoid scrolling to bottom
+	textArea.style.top = '0';
+	textArea.style.left = '0';
+	textArea.style.position = 'fixed';
 
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
 
-  try {
-    const successful = document.execCommand('copy');
-    const msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Fallback: Copying text command was ' + msg);
-  } catch (err) {
-    console.error('Fallback: Oops, unable to copy', err);
-  }
+	try {
+		const successful = document.execCommand('copy');
+		const msg = successful ? 'successful' : 'unsuccessful';
+		console.log('Fallback: Copying text command was ' + msg);
+	} catch (err) {
+		console.error('Fallback: Oops, unable to copy', err);
+	}
 
-  document.body.removeChild(textArea);
+	document.body.removeChild(textArea);
 }
 
 export function copyTextToClipboard(text: string) {
-  if (!navigator.clipboard) {
-    fallbackCopyTextToClipboard(text);
-    return;
-  }
-  navigator.clipboard.writeText(text).then(
-    function () {
-      console.log('Async: Copying to clipboard was successful!');
-    },
-    function (err) {
-      console.error('Async: Could not copy text: ', err);
-    }
-  );
+	if (!navigator.clipboard) {
+		fallbackCopyTextToClipboard(text);
+		return;
+	}
+	navigator.clipboard.writeText(text).then(
+		function () {
+			console.log('Async: Copying to clipboard was successful!');
+		},
+		function (err) {
+			console.error('Async: Could not copy text: ', err);
+		}
+	);
 }

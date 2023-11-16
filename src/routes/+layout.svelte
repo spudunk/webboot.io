f36144be562de7bf50cd1b20f76e8b164d09604e
Christopher Hicks <hickscme@gmail.com>
2023-11-16 10:31:30 -0800

    add resume page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,12 +15,12 @@
 	{@html webManifestLink}
 </svelte:head>
 
-<Header class="fixed z-10 top-0 left-0 w-full" />
+<Header class="fixed z-10 top-0 left-0 w-full print:hidden" />
 <div class="">
 	<main class={`${home ? '' : 'container mt-16'} min-h-[50vh]`}>
 		<slot />
 	</main>
-	<Footer class="mb-8 mt-12" />
+	<Footer class="mb-8 mt-12 print:hidden" />
 </div>
 
 {#await import('$lib/ReloadPrompt.svelte') then { default: ReloadPrompt }}

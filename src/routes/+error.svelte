db40f9743d83193e39a4a0b98c8c6efb7218e3a0
Christopher Hicks <hickscme@gmail.com>
2024-03-11 16:09:15 -0700

    updates

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,6 @@
 	import { page } from '$app/stores';
 </script>
 
-
 <div class={`mt-24 ${$page.url.pathname == '/' ? 'container' : ''}`}>
-  <h1 class="text-2xl">{$page.status} {$page.error?.message}</h1>
+	<h1 class="text-2xl">{$page.status}: {$page.error?.message}</h1>
 </div>

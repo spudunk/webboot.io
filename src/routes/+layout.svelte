060e5277bf4b8a85692d2d2dd927804ad3557bb4
Christopher Hicks <hickscme@gmail.com>
2024-03-11 11:45:11 -0700

    remove vite-pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,8 @@
 	import { page } from '$app/stores';
 	import '../app.css';
 	import '@fontsource-variable/nunito';
-	import { pwaInfo } from 'virtual:pwa-info';
-	// import { onMount } from 'svelte';
-
-	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
-
 	$: home = $page.url.pathname === '/';
 </script>
-
-<svelte:head>
-	{@html webManifestLink}
-</svelte:head>
 
 <Header class="fixed z-10 top-0 left-0 w-full print:hidden" />
 <div class="">
@@ -22,7 +13,3 @@
 	</main>
 	<Footer class="mb-8 mt-12 print:hidden" />
 </div>
-
-{#await import('$lib/ReloadPrompt.svelte') then { default: ReloadPrompt }}
-	<ReloadPrompt />
-{/await}
--- vite.config.ts
+++ vite.config.ts
@@ -1,66 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
-// import { purgeCss } from 'vite-plugin-tailwind-purgecss';
-
 
 export default defineConfig({
-	plugins: [
-		sveltekit(), 
-		SvelteKitPWA({ 
-			registerType: 'autoUpdate' ,
-			includeAssets: [
-				'favicon.ico', 
-				'icons/apple-touch-icon.png', 
-				'favicon-32x32.png',
-				'favicon-16x16.png',
-				// 'mask-icon.svg'
-			],
-			manifest: {
-				"name": "webboot",
-				"short_name": "webboot",
-				"start_url": "/",
-				"id": "/",
-				"theme_color": "#0891B2",
-				"background_color": "#164E63",
-				"display": "standalone",
-				"icons": [
-					{
-						"src": "/icons/192.png",
-						"type": "image/png",
-						"sizes": "192x192"
-					},
-					{
-						"src": "/icons/384.png",
-						"type": "image/png",
-						"sizes": "384x384"
-					},
-					{
-						"src": "/icons/512.png",
-						"type": "image/png",
-						"sizes": "512x512"
-					},
-					{
-						"src": "/icons/1024.png",
-						"type": "image/png",
-						"sizes": "1024x1024"
-					},
-					{
-						"src": "/icons/512-maskable.png",
-						"type": "image/png",
-						"sizes": "512x512",
-						"purpose": "maskable"
-					},
-					{
-						"src": "/icons/144-square.png",
-						"type": "image/png",
-						"sizes": "144x144",
-						"purpose": "any"
-					}
-				],
-			}
-			
-
-		}),
-]
+	plugins: [sveltekit()]
 });
